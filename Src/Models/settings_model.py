39911f9338042457085caca92cdef2153ad31ea4
ilushenka <saharokbro@mail.ru>
2025-10-21 17:49:48 +0800

    prepare for new pull request

--- Src/Models/settings_model.py
+++ Src/Models/settings_model.py
@@ -1,11 +1,9 @@
 from Src.Models.company_model import company_model
 from Src.Core.validator import validator
 from Src.Core.abstract_model import abstract_model
-from Src.Core.responce_format import response_formats
 
 class Settings(abstract_model):
     _company: company_model = None
-    _response_format: response_formats = "json"  # значение по умолчанию json
 
     @property
     def company(self) -> company_model:
@@ -18,13 +16,3 @@ class Settings(abstract_model):
     """
     Формат ответа
     """
-    """Поле формата ответов"""
-
-    @property
-    def response_format(self) -> response_formats:
-        return self._response_format
-
-    @response_format.setter
-    def response_format(self, value: response_formats):
-        validator.validate(value, response_formats, "response format")
-        self._response_format = value
